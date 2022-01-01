5f63a4e0451e00ab509d286c594acc752ebb32d0
krishnabansal89 <83087345+krishnabansal89@users.noreply.github.com>
2022-01-01 18:05:05 +0530

    ?

--- Just_chat/Just_chat/settings.py
+++ Just_chat/Just_chat/settings.py
@@ -134,5 +134,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
 STATICFILES_DIRS = [
-    './Just_chat/Static',
+    os.path.join(BASE_DIR, '../just_chat/static'),
 ]
